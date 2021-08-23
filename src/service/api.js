2e47dd1c9a7c96e269dda911f9aa1ac72d402053
xiaolong Hao <2072451919@qq.com>
2021-08-23 22:03:26 +0800

    添加请求配置

---
+++ src/service/api.js
@@ -0,0 +1,29 @@
+import { get, post } from './request'
+
+const api = {
+  /* 获取仓库版本列表 */
+  repoRelease: () => get('/githubApi/repos/Longgererer/JS-Encoder/releases'),
+  /* 获取仓库最高版本 */
+  repoLatestV: () => get('/githubApi/repos/Longgererer/JS-Encoder/releases/latest'),
+  /* 用户名是否唯一 */
+  usernameUnique: () => get(),
+  /* 邮箱是否唯一 */
+  emailUnique: () => get(),
+  /* 邮箱验证码 */
+  emailAuthCode: () => get(),
+  /* 注册 */
+  signUp: () => { },
+  /* 登录 */
+  login: () => { },
+  /* 向邮箱发送重置密码验证链接 */
+  emailAuthLink: () => { },
+  /* 重置密码 */
+  resetPwd: () => { },
+  /* 发送反馈 */
+  sendFeedback: () => { },
+  /* 查询实例列表 */
+  searchInstanceList: () => { },
+
+}
+
+export default api
---
+++ src/service/request.js
@@ -0,0 +1,109 @@
+/**
+ * 封装axios请求方法
+ */
+
+import axios from 'axios'
+import qs from 'qs'
+import store from '@store/index.js'
+
+axios.defaults.timeout = 10000
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
+
+axios.interceptors.request.use(async config => {
+  config.cancelToken = new axios.CancelToken(cancel => {
+    store.commit('cancelRequest/pushToken', { cancelToken: cancel })
+  })
+  return config
+}, error => {
+  return Promise.reject(error)
+})
+
+axios.interceptors.response.use(
+  response => {
+    if (response.status === 200) {
+      return Promise.resolve(response)
+    } else {
+      return Promise.reject(response)
+    }
+  },
+  error => {
+    if (error.response.status) {
+      switch (error.response.status) {
+        /**
+         * 401: 未登录, 未登录则跳转登录页面，并携带当前页面的路径
+         * 在登录成功后返回当前页面，这一步需要在登录页操作
+         */
+        case 401:
+          router.replace({
+            path: '/login',
+            query: {
+              redirect: router.currentRoute.fullPath
+            }
+          })
+          break
+        /**
+         * 403 token过期
+         * 登录过期对用户进行提示
+         * 清除本地token和清空vuex中token对象
+         * 跳转登录页面
+         */
+        case 403:
+          // 清除token
+          localStorage.removeItem('token')
+          store.commit('loginSuccess', null)
+          // 跳转登录页面，并将要浏览的页面fullPath传过去，登录成功后跳转需要访问的页面 
+          setTimeout(() => {
+            router.replace({
+              path: '/login',
+              query: {
+                redirect: router.currentRoute.fullPath
+              }
+            })
+          }, 1000)
+          break
+        // 404请求不存在
+        case 404:
+          break
+        default:
+      }
+      return Promise.reject(error.response)
+    }
+  }
+)
+
+/**
+ * @param {String} url
+ * @param {Object} params
+ * @param {Object} config
+ * @returns {Promise}
+ */
+export function get (url, params = {}, config = {}) {
+  return new Promise((resolve, reject) => {
+    axios.get(url, {
+      params: params,
+      ...config
+    }).then(res => {
+      resolve(res.data)
+    }).catch(err => {
+      reject(err.data)
+    })
+  })
+}
+
+/** 
+ * @param {String} url
+ * @param {Object} params
+ * @param {Object} config
+ * @returns {Promise}
+ */
+export function post (url, params = {}, config = {}) {
+  return new Promise((resolve, reject) => {
+    axios.post(url, qs.stringify(params), config)
+      .then(res => {
+        resolve(res.data);
+      })
+      .catch(err => {
+        reject(err.data)
+      })
+  });
+}
