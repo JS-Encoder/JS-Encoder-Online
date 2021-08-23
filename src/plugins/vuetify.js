e24be74b4bc5c9ad100c6cbc7f4b0f8a2160db30
xiaolong Hao <2072451919@qq.com>
2021-08-23 22:03:50 +0800

    添加vuetify插件配置

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,18 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: '#1980ff',
+        secondary: '#424242',
+        accent: '#FF4081',
+        error: '#E53935',
+        info: '#272727',
+        success: '#7CB342',
+        warning: '#FFB300',
+      }
+    }
+  }
 });
