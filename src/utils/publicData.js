bb274cb51fe8ef379b019d19d6b2634825843a65
xiaolong Hao <2072451919@qq.com>
2021-08-26 11:58:55 +0800

    添加部分公共数据

---
+++ src/utils/publicData.js
@@ -0,0 +1,17 @@
+/**
+ * 存储常用的静态公共数据
+ */
+export const fontFamList = [
+  'Consolas',
+  'Monaco',
+  'Courier Prime',
+  'JetBrains Mono',
+  'Fira Code',
+  'Hack',
+  'Source Code Pro',
+  'Space Mono',
+  'IBM Plex Mono',
+  'Inconsolata'
+]
+
+export const fontSizeList = [10, 12, 14, 16, 18, 20, 22, 24, 26]
