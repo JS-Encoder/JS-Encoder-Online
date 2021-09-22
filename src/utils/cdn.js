d940cfaabf4ccc1a6d0159e2c89b553039479b54
xiaolong Hao <2072451919@qq.com>
2021-09-22 13:23:03 +0800

    将本地静态脚本和样式上传七牛云

--- src/utils/cdn.js
+++ src/utils/cdn.js
@@ -7,7 +7,7 @@ const externalLinks = {
 
 const iframeLinks = {
   mdCSS: [
-    '/css/markdown-style.css',
+    'http://images.lliiooiill.cn/markdown-style.css',
     'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css'
   ],
   mdJS: [
@@ -15,7 +15,7 @@ const iframeLinks = {
     'https://cdn.bootcdn.net/ajax/libs/flowchart/1.15.0/flowchart.min.js'
   ],
   commonJS: [
-    '/js/common/JSEController.js'
+    'http://images.lliiooiill.cn/JSEController.js'
   ]
 }
 
