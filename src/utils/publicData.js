18db09b2df76d5ece6f33796879df13028da228f
xiaolong Hao <2072451919@qq.com>
2021-09-26 07:44:29 +0800

    替换封面加载图片地址

--- src/utils/publicData.js
+++ src/utils/publicData.js
@@ -32,7 +32,8 @@ const qiNiuImgLink = isProd ? '/images/' : '/images/'
 
 const qiNiuRowLink = 'http://images.lliiooiill.cn/'
 
-const defPosterKey = 'Flxq97KeOG1bd9gMls4YEIRLVttn'
+// 加载实例封面中的图片
+const defPosterKey = '%E5%B0%81%E9%9D%A2%E5%8A%A0%E8%BD%BD%E4%B8%AD.jpg'
 
 export {
   fontFamList,
